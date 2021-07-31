4070d9c1a2b71043c65eafcf726fdda85c3e284f
Jetze Meilink <jetzemeilink@gmail.com>
2021-07-31 20:07:54 +0200

    Movement working and border checking working

--- app.js
+++ app.js
@@ -6,18 +6,67 @@ document.addEventListener('DOMContentLoaded', () => {
   const shapes = [[29,28,9,8], [68,48,28,8],[48,28,9,8],[29,10,9,8], [49,29,28,8]]
   const colors = ['cyan', 'blue', 'orange', 'yellow', 'green', 'red', 'magenta']
   const blocks = []
+  let activeBlock = null
+  const speed = 300
 
   // block class 
 class Block {
   constructor() {
     this.positions = shapes[Math.floor(Math.random() * shapes.length)]
     this.color = colors[Math.floor(Math.random() * colors.length)]
-    
   }
+
+  moveDown() {
+    if (!this.checkHit('down')){
+    this.positions.forEach((item, index) => {
+        let newPos = item += gridSize
+        this.positions[index] = newPos
+    }
+    )
+    this.drawBlock('down')
+  }
+  }
+
+  drawBlock(direction=0) {
+    const directions = {
+      'down': -20,
+      'right': -1,
+      'left': 1
+    }
+    this.positions.forEach(item => {
+      gridArray[item+directions[direction]]?.classList.remove('block') 
+      gridArray[item+directions[direction]]?.classList.remove(this.color) 
+
+    })
+    this.positions.forEach(item => {
+      gridArray[item].classList.add('block')
+      gridArray[item].classList.add(this.color)
+
+    })
+  }
+
+  checkHit(type) {
+    for (let item of this.positions) { 
+      const hitTypes = {
+        'down': item > 380 || gridArray[item+20].classList.contains('fixed-block'),
+        'right': (item +1) % 20 == 0,
+        'left': item % 20 == 0
+      }
+      if (hitTypes[type]) {
+        if (type === 'down') {
+          convertActiveBlock()
+          return true
+
+        }
+        return true 
+    }}
+   
+  }
+
 }
 
 // create grid
-  for (i=0; i < gridSize * gridSize; i++) { 
+  for (let i=0; i < gridSize * gridSize; i++) { 
     div = document.createElement('div')
     div.classList = 'square'
     div.id = `${i}`
@@ -26,53 +75,98 @@ class Block {
   }
 
 // create a new demo block
-function createDemoBlock() {
+function createNewBlock() {
   const block = new Block()
-  blocks.push(block)
+  console.log(block.positions)
+  activeBlock = block
 }
 
-// draw the blocks on the grid
- function drawBlocks(blocks) {
-   for (i=0; i<gridArray.length; i++) {
-     gridArray[i].classList = 'square'
-     for (block of blocks) { 
-        for (el of block.positions) {
-          if (el == i) {
-            gridArray[i].classList = `square block ${block.color}`
-          }
-        }
-     }    
-   }
- }
+// convert active block to fixed
+function convertActiveBlock() {
+    for (let item of activeBlock.positions) {
+    gridArray[item].classList.remove('block')
+    gridArray[item].classList.add('fixed-block')
+    }
+    blocks.push(activeBlock)
+    createNewBlock()
+}
 
-// check if block hits ground 
- function checkHit(array) {
-   for (item of array) {
-       if (item > 380) {
-         return true
-       } else {
-         return false
-     }
-   }
- }
-
-// move the blocks down
- function moveBlocks() {
-  if (!checkHit(block.positions)){
-   blocks.map((block, blockIndex) => {
-     block.positions.map((pos, posIndex) => {
-       let newPos = pos += 20
-       blocks[blockIndex].positions[posIndex] = newPos
-     })
-   })
+// draw all fixed blocks
+function drawFixedBlocks() {
+  if (blocks.length > 0) {
+    blocks.forEach(block => {
+      block.drawBlock()
+    })
   }
-   drawBlocks(blocks)
+  
+}
+
+//  handle move blocks
+function handleMove(e) {
+  switch(e.code) {
+    case 'ArrowLeft':
+      if (!activeBlock.checkHit('left')) {
+        activeBlock.positions.forEach((item, index) => {
+          let newPos = item -= 1
+          activeBlock.positions[index] = newPos
+          activeBlock.drawBlock('left')
+        })
+      }
+      break
+    case 'ArrowRight':
+      if (!activeBlock.checkHit('right')) {
+        activeBlock.positions.forEach((item, index) => {
+          let newPos = item += 1
+          activeBlock.positions[index] = newPos
+          activeBlock.drawBlock('right')
+        })
+      }
+      break
+  }
  }
 
- button.addEventListener('click', moveBlocks)
 
-createDemoBlock()
-drawBlocks(blocks)
+
+// game main
+
+setInterval(() => {
+  activeBlock.moveDown()
+  drawFixedBlocks()
+}, speed)
+    
+
+
+ document.addEventListener('keyup', handleMove)
+ button.addEventListener('click', createNewBlock)
+
+
+
+//  ouwe hit array
+// checkHit(type) {
+//   let hitArray = this.positions.filter(item => { 
+
+//     const hitTypes = {
+//       'down': item > 380 || gridArray[item+20].classList.contains('fixed-block'),
+//       'right': (item +1) % 20 == 0,
+//       'left': item % 20 == 0
+//     }
+//     if (hitTypes[type]) {
+//       if (type === 'down') {
+//         console.log('down')
+//         gridArray[item].classList.remove('block')
+//         gridArray[item].classList.add('fixed-block')
+//         blocks.push(activeBlock)
+//         createNewBlock()
+//       }
+//       return true 
+//   }})
+//   if (hitArray.length > 0) {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+
 
 
 
